04576a16e81400983d6520b749e4036e9e20f136
missaynura <126356820+missaynura@users.noreply.github.com>
2023-02-25 21:36:57 +0000

    Update TestClass.java

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,6 +2,7 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Welcome from Git!");
         System.out.println("Welcome from Github!");
+         System.out.println("Welcome from Github Community!");
 
     }
 }
